6bf544a68cffaa787380dc4ff77c733228f86212
Nathan <nathan.ly.tang@gmail.com>
2020-02-21 14:40:15 -0700

    Draw image and resize canvas based on image size

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById("canvas-draw");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 200, 200);
+var img = new Image();
+img.src = '/images/image1.jpg';
+img.onload = function () {
+    var aspectRatio = img.width / img.height;
+    // Changes the size of the canvas based on the size of the image
+    if (aspectRatio > 1) {
+        canvas.width = canvas.height * aspectRatio;
+        img.width = canvas.width;
+        img.height = canvas.height;
+        console.log(img.width, img.height);
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+    }
+    else if (aspectRatio < 1) {
+        canvas.height = canvas.width / aspectRatio;
+        img.width = canvas.width;
+        img.height = canvas.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+    }
+    else {
+        ctx.drawImage(img, 0, 0);
+    }
+};
