6c03f2accae7c94b486e5a81cb4fea36854daae1
Nathan <nathan.ly.tang@gmail.com>
2020-02-21 23:13:58 -0700

    Draw bounding boxes based on the selected tool

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
+// Initialize canvas
 var canvas = document.getElementById("canvas-draw");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 200, 200);
 var img = new Image();
 img.src = '/images/image1.jpg';
 img.onload = function () {
@@ -24,3 +23,43 @@ img.onload = function () {
         ctx.drawImage(img, 0, 0);
     }
 };
+// Bounding Boxes
+var listOfBoxes = [];
+var startMouseX = 0;
+var startMouseY = 0;
+var endMouseX = 0;
+var endMouseY = 0;
+var toolSelected = null;
+canvas.addEventListener("mousedown", function (event) {
+    if (document.getElementById("button-interesting").checked) {
+        ctx.strokeStyle = "blue";
+        toolSelected = "Interesting";
+    }
+    else if (document.getElementById("button-uninteresting").checked) {
+        ctx.strokeStyle = "red";
+        toolSelected = "Uninteresting";
+    }
+    if (toolSelected != null) {
+        startMouseX = event.x;
+        startMouseY = event.y;
+        startMouseX -= canvas.offsetLeft;
+        startMouseY -= canvas.offsetTop;
+    }
+});
+canvas.addEventListener("mouseup", function (event) {
+    if (toolSelected != null) {
+        endMouseX = event.x;
+        endMouseY = event.y;
+        endMouseX -= canvas.offsetLeft;
+        endMouseY -= canvas.offsetTop;
+        ctx.strokeRect(startMouseX, startMouseY, endMouseX - startMouseX, endMouseY - startMouseY); // Draw rectangle
+        console.log(startMouseX, startMouseY, endMouseX, endMouseY);
+        var box = {
+            startCoors: [startMouseX, startMouseY],
+            endCoors: [endMouseX, endMouseY],
+            toolSelected: toolSelected
+        };
+        listOfBoxes.push(box); // Push box to array listOfBoxes
+        console.log(listOfBoxes);
+    }
+});
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
+// Initialize canvas
+
 var canvas = <HTMLCanvasElement>document.getElementById("canvas-draw");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 200, 200);
 
 var img = new Image();
 img.src = '/images/image1.jpg';
@@ -27,3 +27,48 @@ img.onload = function() {
     }
 
 }
+
+// Bounding Boxes
+
+var listOfBoxes:object[] = []
+var startMouseX:number = 0;
+var startMouseY:number = 0;
+var endMouseX:number = 0;   
+var endMouseY:number = 0;
+var toolSelected:string = null
+
+canvas.addEventListener("mousedown", function(event){
+    // Check which tool is selected
+    if ((document.getElementById("button-interesting") as any).checked) {
+        ctx.strokeStyle = "blue"
+        toolSelected = "Interesting"
+    }
+    else if ((document.getElementById("button-uninteresting") as any).checked) {
+        ctx.strokeStyle = "red"
+        toolSelected = "Uninteresting"
+    }
+    if (toolSelected != null) {
+    startMouseX = event.x;
+    startMouseY = event.y;
+    startMouseX -= canvas.offsetLeft;
+    startMouseY -= canvas.offsetTop;
+    }
+})
+
+canvas.addEventListener("mouseup", function(event) {
+    if (toolSelected != null) {
+    endMouseX = event.x;   
+    endMouseY = event.y;
+    endMouseX -= canvas.offsetLeft;
+    endMouseY -= canvas.offsetTop;
+    ctx.strokeRect(startMouseX, startMouseY, endMouseX-startMouseX, endMouseY-startMouseY); // Draw rectangle
+    var box = { // Create box object with start and end coordinates
+        startCoors: [startMouseX, startMouseY],
+        endCoors: [endMouseX, endMouseY],
+        toolSelected: toolSelected
+    }
+    listOfBoxes.push(box) // Push box to array listOfBoxes
+    console.log(listOfBoxes)
+
+    }
+})
